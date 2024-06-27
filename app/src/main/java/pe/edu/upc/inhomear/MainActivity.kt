1266833d7499aba418302fcef08b8c5b113bfd28
AldoMedina <u202018286@upc.edu.pe>
2024-06-27 00:39:20 -0500

    add: Main menu functionality added.

--- app/src/main/java/pe/edu/upc/inhomear/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/inhomear/MainActivity.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import pe.edu.upc.inhomear.ui.theme.InHomeARTheme
+import pe.edu.upc.inhomear.ui.theme.Translucent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +33,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             InHomeARTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    //color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                    ) {
+                        Menu(modifier = Modifier.align(Alignment.BottomCenter))
+                    }
                 }
             }
         }
@@ -31,17 +49,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Menu(
+    modifier: Modifier
+) {
+    var currentIndex by remember { mutableIntStateOf(0) }
+    val itemsList = listOf(
+        Furniture("Chair", R.drawable.chair),
+        Furniture("Armchair", R.drawable.armchair),
+        Furniture("Desk", R.drawable.desk),
+        Furniture("Table", R.drawable.table),
     )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InHomeARTheme {
-        Greeting("Android")
+    fun updateIndex(offset: Int) {
+        currentIndex = (currentIndex + offset + itemsList.size) % itemsList.size
+    }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        IconButton(onClick = {
+            updateIndex(-1)
+        }) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_keyboard_arrow_left_24),
+                contentDescription = "Back"
+            )
+        }
+
+        FurnitureCard(imageId = itemsList[currentIndex].image)
+
+        IconButton(onClick = {
+            updateIndex(1)
+        }) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_keyboard_arrow_right_24),
+                contentDescription = "Next"
+            )
+        }
     }
 }
+
+@Composable
+fun FurnitureCard(
+    modifier: Modifier = Modifier,
+    imageId: Int,
+) {
+    Box(
+        modifier = modifier
+            .padding(2.dp)
+            .size(100.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .border(width = 1.dp, Translucent, shape = RoundedCornerShape(10.dp))
+    ) {
+        Image(
+            painter = painterResource(id = imageId),
+            contentDescription = "Furniture",
+            contentScale = ContentScale.FillBounds
+        )
+    }
+}
+
+
+data class Furniture(
+    val name: String,
+    val image: Int,
+)
